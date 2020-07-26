25f6e3693229f0ef8028c3ed4050ed4b7388c533
but-sergey <but-sergey@bk.ru>
2020-07-26 17:26:09 +0300

    Обобщенные делегаты

---
+++ Chapter_10/GenericDelegate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GenericDelegate
+{
+    class Program
+    {
+        public delegate void MyGenericDelegate<T>(T arg);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Generic Delegates *****\n");
+
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("Some string data");
+
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            intTarget(9);
+
+            Console.ReadLine();
+        }
+
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine($"arg in uppercase is: {arg.ToUpper()}");
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine($"++arg is: {++arg}");
+        }
+    }
+}
