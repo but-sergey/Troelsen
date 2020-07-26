792af81c851b0fee2163e240e363325f554a8539
but-sergey <but-sergey@bk.ru>
2020-07-26 18:55:11 +0300

    Анонимные методы

--- Chapter_10/CarEvents/Program.cs
+++ Chapter_10/CarEvents/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarEvents
 {
@@ -14,41 +10,25 @@ namespace CarEvents
 
             Car c1 = new Car("SlugBug", 100, 10);
 
-            c1.AboutToBlow += CarIsAlmostDoomed;
-            c1.AboutToBlow += CarAboutToBlow;
+            c1.AboutToBlow += delegate
+            {
+                Console.WriteLine("Eek! Going too fast!");
+            };
 
-            c1.Exploded += CarExploded;
+            c1.AboutToBlow += delegate (object sender, CarEventArgs e)
+            {
+                Console.WriteLine($"Message from Car: {e.msg}");
+            };
 
-            Console.WriteLine("***** Speeding up *****");
-            for (int i = 0; i < 6; i++)
-                c1.Accelerate(20);
+            c1.Exploded += delegate (object sender, CarEventArgs e)
+            {
+                Console.WriteLine($"Fatal Message from Car: {e.msg}");
+            };
 
-            c1.Exploded -= CarExploded;
-
-            Console.WriteLine("\n***** Speeding up *****");
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
 
             Console.ReadLine();
-        }
-
-        public static void CarAboutToBlow(object sender, CarEventArgs e)
-        {
-            if(sender is Car c)
-            {
-                //Car c = (Car)sender;
-                Console.WriteLine($"{c.PetName} says: {e.msg}");
-            }
-        }
-
-        public static void CarIsAlmostDoomed(object sender, CarEventArgs e)
-        {
-            Console.WriteLine($"=> Critical Message form {sender}: {e.msg}");
-        }
-
-        public static void CarExploded(object sender, CarEventArgs e)
-        {
-            Console.WriteLine($"{sender} - {e.msg}");
         }
     }
 }
