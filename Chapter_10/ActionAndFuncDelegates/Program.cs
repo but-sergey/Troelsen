38ba4b51375dd862560ddb9f478419716b471aa4
but-sergey <but-sergey@bk.ru>
2020-07-26 17:34:13 +0300

    Обобщенные делегаты Action<> и Func<> Action<>

---
+++ Chapter_10/ActionAndFuncDelegates/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+        {
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+
+            for(int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(msg);
+            }
+
+            Console.ForegroundColor = previous;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Action and Func *****\n");
+
+            Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
+            actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+
+            Console.ReadLine();
+        }
+    }
+}
