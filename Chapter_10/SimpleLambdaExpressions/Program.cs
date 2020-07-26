e59f313b7a24b4ef296b83d366bf5359f27d6bfc
but-sergey <but-sergey@bk.ru>
2020-07-26 19:17:05 +0300

    Лямбда-выражения: традиционный синтаксис с делегатами

---
+++ Chapter_10/SimpleLambdaExpressions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lambdas *****\n");
+            TraditionalDelegateSyntax();
+            Console.ReadLine();
+        }
+
+        static void TraditionalDelegateSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = list.FindAll(callback);
+
+            Console.WriteLine("Here are you even numbers:");
+            foreach (int evenNumber in evenNumbers)
+            {
+                Console.Write($"{evenNumber}\t");
+            }
+            Console.WriteLine();
+        }
+
+        static bool IsEvenNumber(int i)
+        {
+            return (i % 2) == 0;
+        }
+    }
+}
