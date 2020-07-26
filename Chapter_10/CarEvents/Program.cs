825dcbc649cc0eed0f4b4b7a26e57115f38224ca
but-sergey <but-sergey@bk.ru>
2020-07-26 19:41:54 +0300

    Лямбда-выражения в CarEvents

--- Chapter_10/CarEvents/Program.cs
+++ Chapter_10/CarEvents/Program.cs
@@ -6,32 +6,17 @@ namespace CarEvents
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***** Fun with Events *****\n");
-            int aboutToBlowCounter = 0;
+            Console.WriteLine("***** More Fun with Lambdas *****\n");
 
             Car c1 = new Car("SlugBug", 100, 10);
 
-            c1.AboutToBlow += delegate
-            {
-                aboutToBlowCounter++;
-                Console.WriteLine("Eek! Going too fast!");
-            };
+            c1.AboutToBlow += (sender, e) => { Console.WriteLine(e.msg); };
+            c1.Exploded += (sender, e) => { Console.WriteLine(e.msg); };
 
-            c1.AboutToBlow += delegate (object sender, CarEventArgs e)
-            {
-                aboutToBlowCounter++;
-                Console.WriteLine($"Message from Car: {e.msg}");
-            };
-
-            c1.Exploded += delegate (object sender, CarEventArgs e)
-            {
-                Console.WriteLine($"Fatal Message from Car: {e.msg}");
-            };
-
+            Console.WriteLine("\n***** Speeding up *****");
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
 
-            Console.WriteLine($"AboutToBlow event was fired {aboutToBlowCounter} times.");
             Console.ReadLine();
         }
     }
