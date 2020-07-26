4d7bcc5dd749d444e64cdc05fd361334f20c0d9a
but-sergey <but-sergey@bk.ru>
2020-07-26 17:38:23 +0300

    Func<>

--- Chapter_10/ActionAndFuncDelegates/Program.cs
+++ Chapter_10/ActionAndFuncDelegates/Program.cs
@@ -17,12 +17,30 @@ namespace ActionAndFuncDelegates
             Console.ForegroundColor = previous;
         }
 
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static string SumToString(int x, int y)
+        {
+            return (x + y).ToString();
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Action and Func *****\n");
 
             Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
             actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+
+            Func<int, int, int> funcTarget = new Func<int, int, int>(Add);
+            int result = funcTarget.Invoke(40, 40);
+            Console.WriteLine($"40 + 40 = {result}");
+
+            Func<int, int, string> funcTarget2 = new Func<int, int, string>(SumToString);
+            string sum = funcTarget2(90, 300);
+            Console.WriteLine(sum);
 
             Console.ReadLine();
         }
