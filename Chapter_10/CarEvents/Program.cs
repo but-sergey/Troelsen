f00f9b7e988c134b3182c3bb79258be3865a37af
but-sergey <but-sergey@bk.ru>
2020-07-26 19:01:39 +0300

    Доступ к локальным переменным из анонимного метода

--- Chapter_10/CarEvents/Program.cs
+++ Chapter_10/CarEvents/Program.cs
@@ -7,16 +7,19 @@ namespace CarEvents
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Events *****\n");
+            int aboutToBlowCounter = 0;
 
             Car c1 = new Car("SlugBug", 100, 10);
 
             c1.AboutToBlow += delegate
             {
+                aboutToBlowCounter++;
                 Console.WriteLine("Eek! Going too fast!");
             };
 
             c1.AboutToBlow += delegate (object sender, CarEventArgs e)
             {
+                aboutToBlowCounter++;
                 Console.WriteLine($"Message from Car: {e.msg}");
             };
 
@@ -28,6 +31,7 @@ namespace CarEvents
             for (int i = 0; i < 6; i++)
                 c1.Accelerate(20);
 
+            Console.WriteLine($"AboutToBlow event was fired {aboutToBlowCounter} times.");
             Console.ReadLine();
         }
     }
