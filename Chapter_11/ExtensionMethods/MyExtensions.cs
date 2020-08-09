c8a5463e4c2ff9b0b125bc3bdbef1eabb5e0a7fc
but-sergey <but-sergey@bk.ru>
2020-08-09 20:02:50 +0300

    Fun with Extension Methods

---
+++ Chapter_11/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    static class MyExtensions
+    {
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine($"{obj.GetType().Name} lives here: => {Assembly.GetAssembly(obj.GetType()).GetName().Name}\n");
+        }
+
+        public static int ReverseDigits(this int i)
+        {
+            char[] digits = i.ToString().ToCharArray();
+            Array.Reverse(digits);
+            string newDigits = new string(digits);
+            return int.Parse(newDigits);
+        }
+    }
+}
