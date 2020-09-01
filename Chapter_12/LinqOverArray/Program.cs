976c5bfa4f8dd84cf6d9afb5cc18d9802d5f2d5d
but-sergey <but-sergey@bk.ru>
2020-09-01 21:43:26 +0300

    QueryOverStrings

--- Chapter_12/LinqOverArray/Program.cs
+++ Chapter_12/LinqOverArray/Program.cs
@@ -8,8 +8,24 @@ namespace LinqOverArray
 {
     class Program
     {
+        static void QueryOverStrings()
+        {
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+
+            IEnumerable<string> subset = from g in currentVideoGames
+                                         where g.Contains(" ")
+                                         orderby g
+                                         select g;
+            foreach (string s in subset)
+                Console.WriteLine($"Item: {s}");
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with LINQ to Objects *****\n");
+            QueryOverStrings();
+
+            Console.ReadLine();
         }
     }
 }
