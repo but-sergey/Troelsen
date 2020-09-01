58518671d8f5b2d300689ed8997ed4beea8761f6
but-sergey <but-sergey@bk.ru>
2020-09-01 21:49:24 +0300

    QueryOverStringsWithExtensionMethods

--- Chapter_12/LinqOverArray/Program.cs
+++ Chapter_12/LinqOverArray/Program.cs
@@ -20,10 +20,20 @@ namespace LinqOverArray
                 Console.WriteLine($"Item: {s}");
         }
 
+        static void QueryOverStringsWithExtensionMethods()
+        {
+            string[] currentVideoGames = { "Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2" };
+            IEnumerable<string> subset = currentVideoGames.Where(g => g.Contains(" ")).OrderBy(g => g).Select(g => g);
+
+            foreach (string s in subset)
+                Console.WriteLine($"Item: {s}");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with LINQ to Objects *****\n");
             QueryOverStrings();
+            QueryOverStringsWithExtensionMethods();
 
             Console.ReadLine();
         }
